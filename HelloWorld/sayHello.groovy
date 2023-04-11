23a633a8cad52ca123923846b54a26ba1e2fcb24
Jesper Schjødt <jesper.schjoedt@invixo.com>
2023-04-11 12:52:03 +0200

    Commit message

--- HelloWorld/sayHello.groovy
+++ HelloWorld/sayHello.groovy
@@ -4,7 +4,14 @@ class SayIt {
         println 'Hello World!'
     }
 
+    // is it leap year using calendar function
+    boolean isLeapYear(int year) {
+        Calendar cal = Calendar.getInstance()
+        cal.set(Calendar.YEAR, year)
+        return cal.getActualMaximum(Calendar.DAY_OF_YEAR) > 365
+    }
+
 }
 SayIt si = new SayIt()
 si.sayHello()
-
+println 'Is it leap year: ' + si.isLeapYear(2023)
